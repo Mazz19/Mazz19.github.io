c15a011fa62128fce4a21ab727df4d05ec2c89fc
Mazz19 <mazz19.12.00@gmail.com>
2024-05-25 23:09:39 +0200

    agg

--- django/appointment_booking/bookings/models.py
+++ django/appointment_booking/bookings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} {self.time}"
