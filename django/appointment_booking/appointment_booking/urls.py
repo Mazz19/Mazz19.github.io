c1cc180d125802b7b5a9543f22e2787bb92ebed4
Mazz19 <mazz19.12.00@gmail.com>
2024-05-25 23:36:52 +0200

    agg

--- django/appointment_booking/appointment_booking/urls.py
+++ django/appointment_booking/appointment_booking/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('appointments/', include('bookings.urls')),
+    path('', lambda request: HttpResponseRedirect('/appointments/book/')),  # Reindirizza la radice del sito
 ]
--- django/appointment_booking/bookings/admin.py
+++ django/appointment_booking/bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'date', 'time', 'created_at')
+    search_fields = ('name', 'email', 'phone')
+
+admin.site.register(Appointment, AppointmentAdmin)
