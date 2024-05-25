971ebad525b9c47bb3d912088b5a0947716a08f4
Mazz19 <mazz19.12.00@gmail.com>
2024-05-25 23:14:38 +0200

    agg

--- django/appointment_booking/bookings/views.py
+++ django/appointment_booking/bookings/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AppointmentForm
+from .models import Appointment
 
-# Create your views here.
+def book_appointment(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = AppointmentForm()
+    return render(request, 'bookings/book_appointment.html', {'form': form})
+
+def success(request):
+    return render(request, 'bookings/success.html')
