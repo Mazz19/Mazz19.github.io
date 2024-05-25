61f074a1c17391100ebca4eeef301cbbaaa1193f
Mazz19 <mazz19.12.00@gmail.com>
2024-05-26 00:02:04 +0200

    agg

--- django/appointment_booking/bookings/views.py
+++ django/appointment_booking/bookings/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, redirect
-from .forms import AppointmentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import UserRegisterForm, AppointmentForm
 from .models import Appointment
 
-def book_appointment(request):
+def register(request):
     if request.method == 'POST':
-        form = AppointmentForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('appointments_list')
     else:
-        form = AppointmentForm()
-    return render(request, 'bookings/book_appointment.html', {'form': form})
+        form = UserRegisterForm()
+    return render(request, 'bookings/register.html', {'form': form})
 
-def success(request):
-    return render(request, 'bookings/success.html')
+@login_required
+def appointments_list(request):
+    appointments = Appointment.objects.filter(user=request.user)
+    return render(request, 'bookings/appointments_list.html', {'appointments': appointments})
